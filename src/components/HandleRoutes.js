e2f0c897c625a384d6eb4c52c9f1558ea77ceb36
chaitu047 <chaitu.klk047@gmail.com>
2024-03-31 23:21:32 +0530

    fix - 5

--- src/components/HandleRoutes.js
+++ src/components/HandleRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from './Home';
 import Detail from './Detail';
 import About from './About';
+import {Routes, Route} from 'react-router-dom';
 
 function HandleRoutes() {
     return(
@@ -11,9 +12,11 @@ function HandleRoutes() {
             <li><a href="/About">About</a></li>
             <li><a href="/Detail">Detail</a></li>
         </ul>
-         <Home />
-         <About />
-         <Detail />
+        <Routes>
+            <Route path="/Home" element={Home}></Route>
+            <Route path="/About" element={About}></Route>
+            <Route path="/Detail" element={Detail}></Route>
+        </Routes>
         </>
     )
 }
