e3c57b7ed40aa3d53cc33b1063ba38c61d588d33
chaitu047 <chaitu.klk047@gmail.com>
2024-03-31 22:41:55 +0530

    fix-1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./style.css";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import StateManagement from "./components/StateManagement";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <StateManagement />
       <Footer />
     </div>
   );
