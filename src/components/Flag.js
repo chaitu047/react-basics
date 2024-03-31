fc5281db293d65fe7a42b34bb27fca12bab698d8
chaitu047 <chaitu.klk047@gmail.com>
2024-03-31 22:42:24 +0530

    fix-2

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Counter(props){
     return (
         <div>
-            <label>Flag - {props.value}</label>
+            <label>Flag - {props.value.toString()}</label>
         </div>
     )
 }
